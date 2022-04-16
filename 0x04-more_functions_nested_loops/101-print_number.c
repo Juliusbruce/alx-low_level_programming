4488d1bf85084ecf0ce72f000b808093e0468914
Juliusbruce <brucejulius048@gmail.com>
2022-04-16 12:12:44 -0700

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* print_number - that prints an integer
-*@n: print int
-* Return: Always 0.
+* print_number - a function that prints an integer
+* @n: input
+* Return: inputted figure
 */
 void print_number(int n)
 {
+
 return (0);
 }
