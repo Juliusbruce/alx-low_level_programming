3823eec78da6b58a8eead03a5ef01a89ad3224e2
Juliusbruce <brucejulius048@gmail.com>
2022-04-28 03:44:12 -0700

    my commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,24 +3,22 @@
 /**
  * print_chessboard - print the chessboard, 2d array
  * @a: 2d array of chars
- *Return: void
  */
+
 void print_chessboard(char (*a)[8])
 {
-	/*Declaring variables*/
-	int i, j;
+	int y, x;
 
-	i = 0;
-	while (i < 8) /*number repetitions*/
+	y = 0;
+	while (y < 8)
 	{
-		j = 0;
-		while (j < 8)
+		x = 0;
+		while (x < 8)
 		{
-			_putchar (a[i][j]);
-			j++;
+			_putchar(a[y][x]);
+			x++;
 		}
-		_putchar ('\n'); /*print new line*/
-		i++; /*add +1*/
+		_putchar('\n');
+		y++;
 	}
-
 }
