d447a44c346f8328ee9d35daabf0b11be1f484b8
Juliusbruce <brucejulius048@gmail.com>
2022-05-04 09:52:25 -0700

    my commit

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,1 +1,23 @@
-int _strcmp(char *s1, char *s2);
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: A pointer to a character that will be changed
+ * @s2: A pointer to a character that will also be changed/modified/updated
+ * Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+int i = 0, op = 0;
+
+while (op == 0)
+{
+if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+break;
+op = *(s1 + i) - *(s2 + i);
+i++;
+}
+
+return (op);
+}
