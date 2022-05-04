3f08031de9401d0670a77d2a8e6fa74d982546ff
Juliusbruce <brucejulius048@gmail.com>
2022-05-04 12:21:49 -0700

    my commit

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,44 +1,25 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
- **/
-
+ * _strspn - gets lengthof a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: number of bytes of s in accept
+ */
 unsigned int _strspn(char *s, char *accept)
-
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
+	unsigned int i, j;
 
-	str1 = s;
-	str2 = accept;
-
-	i = 0;
-	while (str1[i] != '\0') /*Declaring WHILE *s */
+	for (i = 0; s[i]; i++)
 	{
-		j = 0;
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		for (j = 0; accept[j]; j++)
 		{
-			if (str2[j] == str1[i]) /*Evaluate condition*/
-			{
-				count++; /*count number*/
+			if (s[i] == accept[j])
 				break;
-			}
-
-			j++;    /*add j+1*/
 		}
-
-		if (s[i] != accept[j]) /*If aren't equals*/
-		{
+		if (!accept[j])
 			break;
-		}
-
-		i++; /*add x+1*/
 	}
-
-	return (count); /*return the value of count*/
+	return (i);
 }
