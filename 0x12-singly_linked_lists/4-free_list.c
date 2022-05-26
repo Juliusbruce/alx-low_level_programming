ee482cee74edef496fc0a4cbab98f69436a39300
Juliusbruce <brucejulius048@gmail.com>
2022-05-26 07:08:50 -0700

    corrected free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,15 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_listint - frees a linked list
- * @head: head of linked list
+ * free_list - frees a list_t list
+ * @head: pointer to the start of the list
+ *
  * Return: void
  */
-void free_listint(listint_t *head)
+void free_list(list_t *head)
 {
-	listint_t *temp;
+listint_t *temp;
 
 	while (head)
 	{
