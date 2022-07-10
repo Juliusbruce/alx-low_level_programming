e632fbada4d53a8b5cff56987281adf55d677227
Juliusbruce <brucejulius048@gmail.com>
2022-07-10 06:50:34 -0700

    added files

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,22 @@
+#include "main2.h"
+
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int main()
+{
+    int a = 12, b = 13;
+    add(a,b);
+    sub(a,b);
+    mul(a,b);
+    div(a,b);
+    mod(a,b);
+
+
+    return (0);
+}
